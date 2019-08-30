a5912b0460ebf3585e1a63834dbacf1a77bdf0b1
ThunderStorms21th <pinakastorm@gmail.com>
2019-08-30 11:05:54 +0200

    delete

--- drivers/motor/max77854_haptic.h
+++
@@ -1,5 +0,0 @@
-#ifndef MOTOR_HAPTIC_H
-#define MOTOR_HAPTIC_H
-
-int setMotorCallback(int (*callback)(int state));
-#endif  // MOTOR_HAPTIC_H
--- include/linux/ion.h
+++
@@ -1,1 +0,0 @@
-../../drivers/staging/android/ion/ion.h
