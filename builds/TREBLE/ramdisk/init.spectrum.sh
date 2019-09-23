b616a6b3371ad4d34c735d1e484da10a324a461c
ThunderStorms21th <pinakastorm@gmail.com>
2019-09-23 14:43:04 +0200

    ramdisk: fix load spectrum profiles

--- builds/TREBLE/ramdisk/init.spectrum.sh
+++ builds/TREBLE/ramdisk/init.spectrum.sh
@@ -3,6 +3,6 @@
 # Profile initialization script by nathanchance
 
 # If there is not a persist value, we need to set one
-if [ ! -f /data/property/persist.spectrum.profile ]; then
+if [ ! grep -q persist.spectrum.profile /data/property/persistent.properties ]; then
     setprop persist.spectrum.profile 0
 fi
